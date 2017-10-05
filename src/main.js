6043b476f377e12847d4b67493242e2c16a75b2d
KIMKIBAEK <kibaek.kimm@gmail.com>
2017-10-05 15:36:05 +0900

    Element-ui Change lang

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue';
 import App from './components/App.vue'
 import store from './store/index'
 import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ko'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI , {locale});
 
 new Vue({
     el : '#app',
